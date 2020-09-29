0b5657c81e022ee609130fb350dd88dd954365ec
techyyy <72086769+techyyy@users.noreply.github.com>
2020-09-29 17:13:14 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <sorting.h>
+#include <dicing.h>
+
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main()  // Nothing to do here
+{
+    // "Everything you can imagine is real." -- Pablo Picasso
+    return 0
+}
